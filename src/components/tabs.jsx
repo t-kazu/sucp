fbb10c197b9362fad210c7f13e01b9e1086f5a2c
t-kazu <s153057@matsu.shimane-u.ac.jp>
2018-07-31 12:58:18 +0900

    タブを作成しました

---
+++ src/components/tabs.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import TopScreen from "../containers/top.jsx";
+
+class TabComponect extends Component {
+  render() {
+    return (
+      <Tabs>
+        <TabList>
+          <Tab>トップ</Tab>
+          <Tab>イラスト</Tab>
+          <Tab>ライセンス</Tab>
+          <Tab>お問い合わせ</Tab>
+        </TabList>
+
+        <TabPanel>
+          <TopScreen />
+        </TabPanel>
+        <TabPanel>
+          <h2>イラスト</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>ライセンス</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>お問い合わせ</h2>
+        </TabPanel>
+      </Tabs>
+    );
+  }
+}
+
+export default TabComponect;
