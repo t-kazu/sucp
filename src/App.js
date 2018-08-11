2ffbc06799bb9f869e74406d3ffee48678337b4c
t-kazu <s153057@matsu.shimane-u.ac.jp>
2018-08-11 16:07:34 +0900

    MDBを導入しました

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { Component } from "react";
 import Menu from "./components/menu-bar/menu-bar.jsx";
 import Footer from "./containers/fotter.jsx";
 
+import "./css/App.css";
+
 class App extends Component {
   render() {
     return (
--- src/containers/fotter.jsx
+++ src/containers/fotter.jsx
@@ -1,50 +1,17 @@
 import React from "react";
-import { Row, Col, Container, Footer } from "mdbreact";
+import { Container, Footer } from "mdbreact";
 
 class FooterPage extends React.Component {
   render() {
     return (
-      <Footer color="indigo" className="font-small pt-0">
-        <Container>
-          <Row>
-            <Col md="12">
-              <div className="mb-5 flex-center">
-                <a className="fb-ic">
-                  <i className="fa fa-facebook fa-lg white-text mr-md-5 mr-3 fa-2x">
-                    {" "}
-                  </i>
-                </a>
-                <a className="tw-ic">
-                  <i className="fa fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x">
-                    {" "}
-                  </i>
-                </a>
-                <a className="gplus-ic">
-                  <i className="fa fa-google-plus fa-lg white-text mr-md-5 mr-3 fa-2x">
-                    {" "}
-                  </i>
-                </a>
-                <a className="li-ic">
-                  <i className="fa fa-linkedin fa-lg white-text mr-md-5 mr-3 fa-2x">
-                    {" "}
-                  </i>
-                </a>
-                <a className="ins-ic">
-                  <i className="fa fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x">
-                    {" "}
-                  </i>
-                </a>
-                <a className="pin-ic">
-                  <i className="fa fa-pinterest fa-lg white-text fa-2x"> </i>
-                </a>
-              </div>
-            </Col>
-          </Row>
-        </Container>
+      <Footer color="light-blue" className="font-small pt-0">
         <div className="footer-copyright text-center py-3">
           <Container fluid>
-            &copy; {new Date().getFullYear()} Copyright:{" "}
-            <a href="https://www.MDBootstrap.com"> MDBootstrap.com </a>
+            &copy; {new Date().getFullYear()} Copyright:
+            <a href="https://shimane-u-characters.com">
+              {" "}
+              島根大学非公認キャラ sucp{" "}
+            </a>
           </Container>
         </div>
       </Footer>
