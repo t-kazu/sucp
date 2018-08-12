27bd84493982736b6bb928b387e32aef532f9079
t-kazu <s153057@matsu.shimane-u.ac.jp>
2018-08-12 15:02:47 +0900

    ページを更新しました

--- src/containers/contact.jsx
+++ src/containers/contact.jsx
@@ -6,7 +6,7 @@ class ContactScreen extends Component {
   render() {
     return (
       <div className="padding-menu">
-        <h2>お問合わせ メールはこちらへ</h2>
+        <h2>お問合わせページは現在作成中です。少々お待ち下さい。</h2>
       </div>
     );
   }
