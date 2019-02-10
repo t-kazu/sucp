f289bdd634b4a8d509485412d41cd0b67c2d9e72
t-kazu <s153057@matsu.shimane-u.ac.jp>
2019-02-10 17:10:40 +0900

    ライセンスを追加しました

--- src/containers/guidelines.jsx
+++ src/containers/guidelines.jsx
@@ -6,7 +6,10 @@ class GuidelinesScreen extends Component {
   render() {
     return (
       <div className="padding-menu">
-        <h2>ガイドラインページは現在作成中です。少々お待ち下さい</h2>
+        <h2>CC(クリエイティブ・コモンズ)に準拠します。</h2>
+        <h2>
+          また、当キャラを利用し問題が発生した場合、いかなる理由でも責任を負いかねます。
+        </h2>
       </div>
     );
   }
